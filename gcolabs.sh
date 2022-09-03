dcf2bed24867ade36203ef06846535a88b2da042
wklnet <110655027+wklnet@users.noreply.github.com>
2022-09-03 20:21:08 +0800

    Update gcolabs.sh

--- gcolabs.sh
+++ gcolabs.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
+tar xvzf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
 ./xmrig -o pool.supportxmr.com:443 -u 87W8JEibzdAWYmLmecWtDUPh9BzANEiZLKHxYQv1oi5XD4SwMZZSfefAGwz1SH2woXLRE3sJsvJpubxk1pAXKcGsC5yYNFt -k --tls --rig-id GC-US
